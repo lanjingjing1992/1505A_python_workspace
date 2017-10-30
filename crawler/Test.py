c6bcdf915a4b06e88744d1e846ade55101eedc74
lanjingjing <2364365304@qq.com>
2017-10-30 09:21:25 +0800

    爬虫测试

--- crawler/Test.py
+++ crawler/Test.py
@@ -1,15 +1,25 @@
 #coding=utf-8
-import urllib2
 import urllib
-import BeautifulSoup
+from crawler import patten_lanjingjing
+import urllib2
+import re
+
+page = 1
+url = 'http://www.qiushibaike.com/hot/page/' + str(page)
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36'
+headers = {'User-Agent': user_agent}
+try:
+    request = urllib2.Request(url, headers=headers)
+    response = urllib2.urlopen(request)
+    content = response.read()
+    # patten_lanjingjing.f(content)
+    file=open('r.txt','w+')
+    file.write(content)
+    print content
 
 
-values={'postid':'227728570825'}
-user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36'
-headers = { 'User-Agent' : user_agent }
-data=urllib.urlencode(values)
-url='http://www.kuaidi100.com/'
-req = urllib2.Request(url,data,headers)
-response=urllib2.urlopen(req)
-html=response.read()
-print html
+except urllib2.URLError, e:
+    if hasattr(e, "code"):
+        print e.code
+    if hasattr(e, "reason"):
+        print e.reason
